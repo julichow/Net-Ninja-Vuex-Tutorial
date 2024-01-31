d49c00ec26af107447699467ef4b817561ae686f
julichow <julichow94@gmail.com>
2024-02-01 01:21:22 +0800

    add lesson 4

---
+++ Net-Ninja-Vuex-Tutorial/#5 - getters/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <product-list-one />
+    <product-list-two />
+  </div>
+</template>
+
+<script>
+//import
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
+export default {
+  name: "App",
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  data() {
+    return {
+    
+    };
+  },
+};
+</script>
+
+<style></style>

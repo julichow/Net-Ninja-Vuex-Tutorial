a07b0c4542b50be5532043eb0d9cb64c5a83e459
julichow <julichow94@gmail.com>
2024-01-31 23:43:56 +0800

    add lesson 2

---
+++ Net-Ninja-Vuex-Tutorial/#2 - a simple vue app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <product-list-one :products="products"/>
+    <product-list-two :products="products"/>
+  </div>
+</template>
+
+<script>
+//import
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
+export default {
+  name: "App",
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  data() {
+    return {
+      products: [
+        { name: "Banana Skin", price: 20 },
+        { name: "Shiny Star", price: 40 },
+        { name: "Green Shells", price: 60 },
+        { name: "Red Shells", price: 80 },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
